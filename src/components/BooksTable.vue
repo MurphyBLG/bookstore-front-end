02cd791ba813b52e418e03b565185dc7f93fcf06
vitaliy.ryavkin <vitaliy.ryavkin@gmail.com>
2022-10-18 21:44:54 +0500

    Pagination ready

---
+++ src/components/BooksTable.vue
@@ -0,0 +1,114 @@
+<template>
+    <table class="books_table">
+        <thead>
+          <tr>
+            <th>Book name</th>
+            <th>Author</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+
+          <tr v-for="record in recordsToShow" :key="record.BookId">
+            <td>{{record.name}}</td>
+            <td>{{record.author}}</td>
+            <td>{{record.price}}</td>
+          </tr>
+
+        </tbody>
+    </table>
+
+    <div class="books_table_pagination">
+      <MyBtn class="go_to_left_btn" btnText="<-" @click="previousPage()"/>
+      <MyBtn class="go_to_left_btn" :btnText="totalPage" />
+      <MyBtn class="go_to_right_btn" btnText="->" @click="nextPage()"/>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import  MyBtn from './MyBtn.vue'
+
+export default {
+    data() {
+        return {
+            records: [],
+            page: 0,
+            recordsPerPage: 5
+        };
+    },
+    components: { MyBtn },
+    methods: {
+        loadData() {
+          axios.get("https://localhost:7147/api/Book", { crossDomain: true }).then((response) => {
+            this.records = response.data;
+          });
+        },
+        nextPage() {
+            // Добавить ошибку > количества доступных страниц
+            this.page++;
+        },
+        previousPage() {
+            // Добавить ошибку <0
+            this.page--;
+        }
+    },
+    beforeMount() {
+      this.loadData();
+    },
+    computed: {
+      recordsToShow() {
+        return this.records.slice(this.page * this.recordsPerPage, Math.min(this.page * this.recordsPerPage + 5, this.records.length));
+      },
+      pages() {
+        return Math.ceil(this.records.length / this.recordsPerPage); 
+      },
+      totalPage() {
+        return `Page ${this.page + 1} of ${this.pages}`;
+      }
+    }
+}
+</script>
+
+<style>
+  .books_table {
+    border-collapse: collapse;
+
+    margin: 10px 0;
+    border-radius: 5px 5px 0 0;
+    overflow: hidden;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  }
+
+  .books_table thead tr {
+    background-color: #D0B097;
+    color: white;
+
+    text-align: left;
+    font-weight: bold;
+  } 
+
+  .books_table th, 
+  .books_table td {
+    padding: 12px 15px;
+  }
+
+  .books_table tbody tr {
+    border-bottom: 1px solid #E5CEBC;
+  }
+
+  .books_table tbody tr:nth-of-type(even) {
+    background-color: #F5E9E0;
+  }
+
+  .books_table tbody tr:last-of-type {
+    border-bottom: 2px solid #D0B097;
+  }
+
+  .books_table_pagination {
+    display: inline-flex;
+    justify-content: space-between;
+    height: 25px;
+  }
+</style>
--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -1,57 +1,18 @@
 <template>
   <div class="books_table_wrapper">
     <div class="grid_wrapper">
-      
       <BooksTableControllPanel />
-    
-      <table class="books_table">
-        <thead>
-          <tr>
-            <th>Book name</th>
-            <th>Author</th>
-            <th>Price</th>
-          </tr>
-        </thead>
       
-        <tbody>
-
-          <tr v-for="record in records" :key="record.BookId">
-            <td>{{record.name}}</td>
-            <td>{{record.author}}</td>
-            <td>{{record.price}}</td>
-          </tr>
-
-        </tbody>
-      </table>
-
-      <BooksTablePagination :recordsCnt="records.length"/>
+      <BooksTable />
     </div>
-    
   </div>
-  
 </template>
 
 <script>
-import axios from 'axios'
 import BooksTableControllPanel from '@/components/BooksTableControllPanel.vue';
-import BooksTablePagination from '@/components/BooksTablePagination.vue';
+import BooksTable from '@/components/BooksTable.vue';
 export default {
-    data() {
-        return {
-            records: []
-        };
-    },
-    methods: {
-        loadData() {
-          axios.get("https://localhost:7147/api/Book", { crossDomain: true }).then((response) => {
-            this.records = response.data;
-          });
-        }
-    },
-    beforeMount() {
-      this.loadData();
-    },
-    components: { BooksTableControllPanel, BooksTablePagination }
+  components: { BooksTableControllPanel, BooksTable }
 }
 </script>
 
@@ -64,40 +25,6 @@ export default {
     height: auto;
     margin: auto;
     margin-top: 100px;
-  }
-
-  .books_table {
-    border-collapse: collapse;
-
-    margin: 10px 0;
-    border-radius: 5px 5px 0 0;
-    overflow: hidden;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-  }
-
-  .books_table thead tr {
-    background-color: #D0B097;
-    color: white;
-
-    text-align: left;
-    font-weight: bold;
-  } 
-
-  .books_table th, 
-  .books_table td {
-    padding: 12px 15px;
-  }
-
-  .books_table tbody tr {
-    border-bottom: 1px solid #E5CEBC;
-  }
-
-  .books_table tbody tr:nth-of-type(even) {
-    background-color: #F5E9E0;
-  }
-
-  .books_table tbody tr:last-of-type {
-    border-bottom: 2px solid #D0B097;
   }
 
   .grid_wrapper {
